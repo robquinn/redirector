daa1ecbfe40b9b8d22819c1d5b0b69412cb8c57e
Robert Quinn <robert.quinn@russlyon.com>
2023-06-23 14:43:15 -0700

    heroku port 2

--- src/server/index.ts
+++ src/server/index.ts
@@ -3,14 +3,17 @@ import httpServer from './express/server'
 // Modified server startup
 const startServer = async (): Promise<void> => {
   await new Promise<void>((resolve) => {
-    httpServer.listen({ port: process.env.SERVER__PORT ?? 80 }, () => {
-      console.log(
-        `🚀 Server ready at http://${process.env.SERVER__HOSTNAME}:${
-          process.env.SERVER__PORT ?? 80
-        }/`,
-      )
-      resolve()
-    })
+    httpServer.listen(
+      { port: process.env.PORT ?? process.env.SERVER__PORT ?? 80 },
+      () => {
+        console.log(
+          `🚀 Server ready at http://${process.env.SERVER__HOSTNAME}:${
+            process.env.PORT ?? process.env.SERVER__PORT ?? 80
+          }/`,
+        )
+        resolve()
+      },
+    )
   })
 }
 
