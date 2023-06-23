9fc6fadde2eb05663b0761c77036d5316286d93d
Robert Quinn <robert.quinn@russlyon.com>
2023-06-23 12:52:52 -0700

    env vars change change

--- src/server/graphql/context.ts
+++ src/server/graphql/context.ts
@@ -1,15 +1,6 @@
 import { PrismaClient } from '@prisma/client'
-import isProd from '../utils/is-prod'
 
-export const prisma = new PrismaClient({
-  datasources: {
-    db: {
-      url: isProd()
-        ? process.env.DATABASE__DB_URL_PROD
-        : process.env.DATABASE__DB_URL_DEV,
-    },
-  },
-})
+export const prisma = new PrismaClient({})
 
 export interface Context {
   prisma: PrismaClient
