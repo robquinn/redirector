0eed33b0a86fa37d0aa1cf7a251f621899398239
Robert Quinn <robert.quinn@russlyon.com>
2023-06-23 12:20:39 -0700

    graphql codegen path 2

--- codegen.ts
+++ codegen.ts
@@ -1,8 +1,13 @@
+import * as path from 'path'
 import { type CodegenConfig } from '@graphql-codegen/cli'
 
+const pathToSchema = path.resolve(
+  process.cwd(),
+  'src/server/graphql/generated/schema.graphql',
+)
 const config: CodegenConfig = {
   overwrite: true,
-  schema: './src/server/graphql/generated/schema.graphql', // GraphQL endpoint
+  schema: pathToSchema, // GraphQL endpoint
   documents: './src/server/graphql/**.ts', //  parse graphql operations in matching files
   generates: {
     './src/server/graphql/generated/graphql.ts': {
